f0c73b3aeb0e37bbbebfafeb56219cbe2b9379a5
shakibdshy <shakibdshy@gmail.com>
2018-10-10 19:45:41 +0600

    Service Slider Update

---
+++ active.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    //Slider Area
+    $('.slider-active').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+    //Service Area
+    $('.service-active').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    })
+});
