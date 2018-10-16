f4d9b8c8e6038eabd40e41ddf80f65053d912ef0
shakibdshy <shakibdshy@gmail.com>
2018-10-16 18:08:15 +0600

    Project Update

--- active.js
+++ active.js
@@ -35,4 +35,68 @@ $(document).ready(function () {
             }
         }
     })
+
+    // Project Area
+    var $grid = $('.project-masonry').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            // use outer width of grid-sizer for columnWidth
+            columnWidth: 1
+        }
+    })
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
