1f0aa37414ceaa4c0ad8853364962c2a4996b065
shakibdshy <shakibdshy@gmail.com>
2018-10-20 14:44:57 +0600

    Pricing Area Update

--- active.js
+++ active.js
@@ -85,6 +85,26 @@ $(document).ready(function () {
             }
         }
     })
+
+    //Pricing Area
+    $('.pricing-active').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        navText: ['<i class ="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i>'],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    })
     
 
 
